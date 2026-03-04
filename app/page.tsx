5cfefb1ac481729a33ed13ef1ff27cf3c716f0a6
Seth Villa <sethvilla13@gmail.com>
2026-03-04 00:09:37 -0800

    add basic UI flex layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-"use client";
-import Image from "next/image";
-import axios from "axios";
-import { useEffect } from "react";
-export default function Home() {
 
-  useEffect(() => {
-    axios.post("/api/translation", {
-      text: "Hello, world!",
-    }).then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-
+export default function Page() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col min-h-screen gap-8">
+
+      <div className="shrink-0 flex gap-4 items-end justify-center min-h-[calc(42svh)]">
+        <h1 className="text-2xl font-bold">Resilient Labs</h1>
+        <span className="bg-red-500 h-7 w-[1px] opacity-90"></span>
+        <span className="text-2xl">Breaking language barriers.</span>
+      </div>
+
+      <div className="flex-1 flex items-start justify-center">
+        <div className="w-full max-w-2xl rounded-[28px] border border-zinc-300 dark:border-zinc-700 p-4"></div>
+      </div>
+
     </div>
   );
 }
