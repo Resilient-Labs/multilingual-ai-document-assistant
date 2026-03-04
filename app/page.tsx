277be2f731683f3faef7faf099b2cd864021553a
Seth Villa <sethvilla13@gmail.com>
2026-03-04 09:13:38 -0800

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex flex-col min-h-screen gap-8">
 
       <div className="shrink-0 flex gap-4 items-end justify-center min-h-[calc(42svh)]">
-        <h1 className="text-2xl font-bold">Resilient Labs</h1>
+        <h1 className="text-2xl font-bold flex items-end gap-2 justify-center">          <Image
+            src="/logo.svg"
+            alt="Resilient Labs"
+            width={100}
+            height={100}
+            className="h-10 w-auto rounded-full translate-y-1"
+          />Resilient Labs</h1>
         <span className="bg-red-500 h-7 w-[1px] opacity-90"></span>
         <span className="text-2xl">Breaking language barriers.</span>
       </div>
