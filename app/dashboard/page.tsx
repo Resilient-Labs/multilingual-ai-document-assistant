464cd3ff24ad933551275fc11713e09a41dd1309
Seth Villa <sethvilla13@gmail.com>
2026-03-10 10:39:29 -0700

    feat: initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function Page() {
+
+  return (
+    <div className="flex h-screen">
+      <div className="flex-1">
+        Left
+      </div>
+      <div className="flex-1">
+        Right
+      </div>
+    </div>
+  );
+}
